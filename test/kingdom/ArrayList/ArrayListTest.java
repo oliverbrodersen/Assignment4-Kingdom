9e5fbe4a03c934109d35c256bdd476e78e1adc15
Benjamin <benjamin.preece@hotmail.com>
2020-05-07 09:56:20 +0200

    Testing

--- test/kingdom/ArrayList/ArrayListTest.java
+++ test/kingdom/ArrayList/ArrayListTest.java
@@ -64,46 +64,159 @@ class ArrayListTest {
     }
 
     @Test
-    void testAdd() {
+    void tryingToGetElementAtCorrectIndex()
+    {
+        list.add("Test");
+        Assertions.assertEquals("Test",list.get(0));
+    }
+
+
+    @Test
+    void tryingToRemoveElementAtWrongIndex()
+    {
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () ->
+        {
+            list.remove(5);
+        });
     }
 
     @Test
-    void set() {
+    void tryingToRemoveSpecificElementThatDoesntExist()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+        Assertions.assertThrows(IllegalStateException.class, () ->
+        {
+            list.remove("Test6");
+        });
     }
 
     @Test
-    void get() {
+    void tryingToRemoveSpecificElementThatDoesExist()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+            list.remove("Test2");
+        Assertions.assertNotEquals("Test2",list.get(2));
     }
 
     @Test
-    void remove() {
+    void tryingToRemoveElementAtCorrectIndex()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+        list.remove(2);
+        Assertions.assertNotEquals("Test2",list.get(2));
     }
 
     @Test
-    void testRemove() {
+    void gettingIndexOfUnspecifiedElement()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+        Assertions.assertEquals(-1,list.indexOf("Test10"));
     }
 
     @Test
-    void indexOf() {
+    void gettingIndexOfElement()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+        Assertions.assertEquals(2,list.indexOf("Test2"));
     }
 
     @Test
-    void contains() {
+    void gettingTrueContainsElement()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+        Assertions.assertTrue(list.contains("Test2"));
     }
 
     @Test
-    void isEmpty() {
+    void gettingFalseContainsElement()
+    {
+        for (int i = 0; i < 5; i++) {
+            list.add("Test" + i);
+        }
+        Assertions.assertFalse(list.contains("Test10"));
     }
 
     @Test
-    void isFull() {
+    void isEmptyWhenFull()
+    {
+        for (int i = 0; i < 100; i++) {
+            list.add("Test");
+        }
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @Test
-    void size() {
+    void isEmptyWhenHalfFull()
+    {
+        for (int i = 0; i < 50; i++) {
+            list.add("Test");
+        }
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @Test
-    void testToString() {
+    void isEmptyWhenEmpty()
+    {
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    @Test //Method always returns true
+    void isFullWhenFull()
+    {
+        for (int i = 0; i < 100; i++) {
+            list.add("Test");
+        }
+        Assertions.assertTrue(list.isFull());
+    }
+
+    @Test //Method always returns true
+    void isFullWhenHalfFull()
+    {
+        for (int i = 0; i < 50; i++) {
+            list.add("Test");
+        }
+        Assertions.assertFalse(list.isFull());
+    }
+
+    @Test //Method always returns true
+    void isFullWhenEmpty()
+    {
+        Assertions.assertFalse(list.isFull());
+    }
+
+    @Test
+    void testingSizeWhenEmpty()
+    {
+        Assertions.assertEquals(0,list.size());
+    }
+
+    @Test
+    void testingSizeWhenOverFull()
+    {
+        for (int i = 0; i < 101; i++) {
+            list.add("Test");
+        }
+        Assertions.assertEquals(101,list.size());
+    }
+
+    @Test
+    void testingSizeIsCorrect()
+    {
+        for (int i = 0; i < 7; i++) {
+            list.add("Test");
+        }
+        Assertions.assertEquals(7,list.size());
     }
 }
